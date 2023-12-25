faac8740462e6911d288d5746bb281bc7e8409f5
hlfr07 <hf46078@gmail.com>
2023-12-25 15:37:04 -0500

    ya esta la base en la nube

--- database/conexion.js
+++ database/conexion.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2'); // o 'mysql' si prefieres
 
+/*
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
@@ -7,7 +8,15 @@ const pool = mysql.createPool({
   port: 3306,
   database: 'cholon'
 });
+*/
 
+const pool = mysql.createPool({
+  host: 'bfncbsukldeaquuuktbf-mysql.services.clever-cloud.com',
+  user: 'ubvkgh4bibh2rvnx',
+  password: 'icBjIC74iTMTQfk6roqn',
+  port: 3306,
+  database: 'bfncbsukldeaquuuktbf'
+});
 
 // Exporta el pool para que pueda ser utilizado desde otros módulos
 module.exports = pool;
